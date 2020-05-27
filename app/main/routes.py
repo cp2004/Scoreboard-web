5d7aac8fc288be8cca560af610a10757706929a4
cp2004 <charlie.powell@gmail.com>
2020-05-27 18:59:27 +0100

    Reverse order of games (Newest to oldest)

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,7 +14,7 @@ def index():
         currentSession = session.getSessionId()
     games = game_data.getIndex()
 
-    return render_template('main/index.html', title="Home", currentSession=currentSession, games=games, game_data=game_data, User=User )
+    return render_template('main/index.html', title="Home", reversed=reversed, currentSession=currentSession, games=games, game_data=game_data, User=User )
 
 
 @bp.route('/user/<username>')
@@ -22,7 +22,7 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     games = game_data.loadUser(user.id)['games']
 
-    return render_template('main/user.html', user=user, title=user.username, games=games, game_data=game_data, User=User)
+    return render_template('main/user.html', user=user, reversed=reversed, title=user.username, games=games, game_data=game_data, User=User)
 
 @bp.route('/delete/<id>')
 def delete_game(id):
