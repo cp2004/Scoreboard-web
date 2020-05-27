84b0aa29f43d06123061c39bfda5a3fb143f67b1
cp2004 <charlie.powell@gmail.com>
2020-05-27 14:50:42 +0100

    View previous games for user and global (+delete)

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,31 @@
-from app import db
+from app import db, session, game_data
 from app.main import bp
-from flask import url_for, render_template, request
+from flask import url_for, render_template, request, redirect
 from flask_login import current_user, login_required, login_user, logout_user
+from app.models import User
 
 @bp.route('/')
 @bp.route('/index')
 @login_required
 def index():
-    try:
-        return render_template('main/index.html', title="home")
-    except:
-        return "Game server unwell"
+    if session.is_active():
+        currentSession = None
+    else:
+        currentSession = session.getSessionId()
+    games = game_data.getIndex()
+
+    return render_template('main/index.html', title="Home", currentSession=currentSession, games=games, game_data=game_data, User=User )
+
+
+@bp.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    games = game_data.loadUser(user.id)['games']
+
+    return render_template('main/user.html', user=user, title=user.username, games=games, game_data=game_data, User=User)
+
+@bp.route('/delete/<id>')
+def delete_game(id):
+    game_data.deletegame(id)
+    #Redirect to previous page?
+    return redirect(url_for('main.index'))
