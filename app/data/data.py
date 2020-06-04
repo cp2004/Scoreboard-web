315777b57c95ac0f4642eab5727b83170d28d2c3
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-06-04 12:12:09 +0100

    Add docstrings to datamanager

--- app/data/data.py
+++ app/data/data.py
@@ -3,7 +3,14 @@ import json
 
 
 class DataManager():
+    """Manage reading and writing data
+    """
     def __init__(self, app):
+        """Initialise the manager
+
+        Args:
+            app (Flask): App instance so config is accessible
+        """
         self.basedir = os.path.dirname(__file__)
         self.DATA_DIR = os.path.join(self.basedir, app.config['DATA_DIRECTORY'])
         self.testing = app.config['TESTING']
@@ -12,6 +19,14 @@ class DataManager():
             os.mkdir(self.DATA_DIR)
 
     def readFile(self, name, dir=None):
+        """Load a json file from disk
+
+        Args:
+            name (str): filename, no extension
+            dir (str, optional): Sub directory to use. Defaults to None.
+        Returns:
+            dict: dict generated from json file on disk
+        """
         try:
             with open(self.getPath(name, dir), 'r') as file:
                 data = json.load(file)
@@ -21,6 +36,13 @@ class DataManager():
             return None
 
     def writeFile(self, name, data, dir=None):
+        """Write a json file to disk
+
+        Args:
+            name (str): filename, no extension
+            data (dict): data to write
+            dir (str, optional): Name of directory to write to. Defaults to None.
+        """
         if dir:
             if not os.path.isdir(os.path.join(self.DATA_DIR, dir)):
                 os.makedirs(os.path.join(self.DATA_DIR, dir))
@@ -29,6 +51,12 @@ class DataManager():
             json.dump(data, file)
 
     def deleteFile(self, name, dir=None):
+        """Deletes the file specified from disk
+
+        Args:
+            name (str): filename, no extension
+            dir (str, optional): Name of directory that file is in. Defaults to None.
+        """
         path = self.getPath(name, dir)
         if self.file_exists(name, dir='games'):
             os.remove(path)
@@ -36,9 +64,28 @@ class DataManager():
             print("File not found")
 
     def file_exists(self, name, dir=None):
+        """Checks if the file specified exists
+
+        Args:
+            name (str): filename, no extension
+            dir (str, optional): Name of directory to look in. Defaults to None.
+
+        Returns:
+            bool: True if file exists, false if not
+        """
         return os.path.isfile(self.getPath(name, dir))
 
     def getPath(self, name, dir=None, filetype='.json'):
+        """Joins the path to the file using system separators
+
+        Args:
+            name (str): filename
+            dir (str, optional): Name of directory. Defaults to None.
+            filetype (str, optional): Extension of file. Defaults to '.json'.
+
+        Returns:
+            str: String of path to file
+        """
         if dir:
             if filetype:
                 return os.path.join(self.DATA_DIR, dir, '{}{}'.format(name, filetype))
@@ -51,6 +98,11 @@ class DataManager():
                 return os.path.join(self.DATA_DIR, '{}'.format(name))
 
     def remove_all(self, testing=False):
+        """Remove all data: ONLY USED FOR TESTING, checks config as well.
+
+        Args:
+            testing (bool, optional): Are you testing. Defaults to False.
+        """
         if testing:  # Sanity check, should never be used in production
             if self.testing:  # Second check if testing in config
                 # Remove Games
